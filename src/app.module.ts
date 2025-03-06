c8cccc0a9700164f43b76cb90458381ee1634287
André Fernandes <andreluissilvafernandes@gmail.com>
2025-03-05 23:28:46 -0300

    UPDATE: app module dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './Auth/Modules/auth.module';
 import { UserModule } from './User/Modules/user.module';
+import { StudyModule } from './Study/Modules/study.module';
+import { GeminiModule } from './Gemini/Modules/gemini.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, StudyModule, GeminiModule],
   controllers: [],
   providers: [],
 })
